6068610b402d7f0caaca7934970ed04bd78ed6c1
Ernesto Mejias <ernepazzo1212@gmail.com>
2023-07-09 00:16:13 -0400

    Quitando el heavy_tasks_controller.rb de tener k estar authenticate!!!

--- app/controllers/heavy_tasks_controller.rb
+++ app/controllers/heavy_tasks_controller.rb
@@ -1,5 +1,5 @@
 class HeavyTasksController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def create
     HeavyTaskJob.perform_later(current_user.id)
