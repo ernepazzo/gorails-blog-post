b913c7ecad08a2c891443a5fed9aacbc6c8d0a53
Ernesto Mejias <ernepazzo1212@gmail.com>
2023-07-08 22:25:25 -0400

    Creando el BlogPost de GoRails

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :blog_posts
+
+  # Defines the root path route ("/")
+  root "blog_posts#index"
+
+
+  resources :heavy_tasks, only: :create
+end
